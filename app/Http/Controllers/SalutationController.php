6e03374d2dbff12525d142d2d5d14de9fa3d363c
MARSOC <chriskilelo@gmail.com>
2025-04-18 18:49:35 +0300

    Create empty files for the Salutations Module     - Salutation  Model [app/Models/Salutation.php]     - Salutation  Factory [database/factories/SalutationFactory.php]     - Salutation  Migration [database/migrations/2025_04_18_153742_create_salutations_table.php]     - Salutation  Request [app/Http/Requests/StoreSalutationRequest.php]     - Salutation  Request [app/Http/Requests/UpdateSalutationRequest.php]     - Salutation  Controller [app/Http/Controllers/SalutationController.php]

---
+++ app/Http/Controllers/SalutationController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreSalutationRequest;
+use App\Http\Requests\UpdateSalutationRequest;
+use App\Models\Salutation;
+
+class SalutationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreSalutationRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Salutation $salutation)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Salutation $salutation)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateSalutationRequest $request, Salutation $salutation)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Salutation $salutation)
+    {
+        //
+    }
+}
